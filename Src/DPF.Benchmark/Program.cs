ab645821b07aa0f33f217ac3fc96a08c829932e8
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2018-10-21 09:21:10 +0200

    Args run

--- Src/DPF.Benchmark/Program.cs
+++ Src/DPF.Benchmark/Program.cs
@@ -10,7 +10,7 @@ namespace DPF.Benchmark
             {
                 typeof(SwappingBenchmark),
                 typeof(CountingEvenNumbers),
-            });
+            }).Run(args);
         }
     }
 }
